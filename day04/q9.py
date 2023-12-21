f98829509a58404c163ef758cdf7c77cacb99623
matthieu <matthieu.gaudry78@gmail.com>
2023-12-21 14:37:31 +0100

    d4/q9

---
+++ day04/q9.py
@@ -0,0 +1,24 @@
+#Implement a function to check if a given year is a leap year or not. 
+
+def leap_year(num):
+    return num % 4 == 0 and num % 100 != 0 or num % 400 == 0
+
+def valid_year():
+    try:
+        number = int(input("Enter an number : "))
+        return(number)
+            
+    except ValueError:
+        print("That's not an integer. Please enter a valide integer.")
+        valid_year()
+
+def main():
+    num = valid_year()
+    result = leap_year(num)
+    if result == True:
+        print("%d is a leap year." % num)
+    else :
+        print("%d is not a leap year." % num)
+
+if __name__ == "__main__":
+    main()
