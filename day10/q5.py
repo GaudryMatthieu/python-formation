d969b36c8f9cdad811cc3a3d549b3af496e0abbd
matthieu <matthieu.gaudry78@gmail.com>
2024-07-01 21:25:22 +0200

    d11/q1

---
+++ day10/q5.py
@@ -0,0 +1,8 @@
+# Given a JSON file with product details (name, price, quantity), create a Product class with encapsulated attributes
+
+def main():
+    #none
+    return 0
+    
+if  __name__ == "__main__":
+    main()
---
+++ day11/q1.py
@@ -0,0 +1,10 @@
+# Given a list of numbers, create a NumPy array and find the sum and product of its elements
+
+import numpy as np
+
+def main():
+    arr = np.array([1, 2, 3, 4, 5])
+    print(arr)
+
+if __name__ == '__main__':
+    main()
